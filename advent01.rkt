30a330adce67cdb9f8fed234c61d2d6e89db1fe3
Filip Lajszczak <filip@lajszczak.dev>
2021-12-19 21:03:21 +0000

    count identity instead of sum of ones

--- advent01.rkt
+++ advent01.rkt
@@ -3,14 +3,11 @@
 
 (define input (map string->number (file->lines "day01.in")))
 
-(define (one? l r)
-  (if (< l r) 1 0))
-
 (define (sums-of-sliding-triads lst)
   (zip-with + (init (init lst)) (init (cdr lst)) (cddr lst)))
 
 ;; Part 1
-(display (sum (adjacent-map input one?)))
+(display (count identity (adjacent-map input <)))
 (newline)
 ;; Part 2
-(display (sum (adjacent-map (sums-of-sliding-triads input) one?)))
+(display (count identity (adjacent-map (sums-of-sliding-triads input) <)))
