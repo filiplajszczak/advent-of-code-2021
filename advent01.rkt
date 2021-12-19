4a7b4737e7e3605fb5dfed0b979a9619efac5c02
Filip Lajszczak <filip@lajszczak.dev>
2021-12-19 20:09:43 +0000

    if instead of cond

--- advent01.rkt
+++ advent01.rkt
@@ -4,8 +4,7 @@
 (define input (map string->number (file->lines "day01.in")))
 
 (define (one? l r)
-  (cond [(< l r) 1]
-        [else 0]))
+  (if (< l r) 1 0))
 
 (define (sums-of-sliding-triads lst)
   (zip-with + (init (init lst)) (init (cdr lst)) (cddr lst)))
